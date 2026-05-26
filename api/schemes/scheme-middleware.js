8a3da3dcb42553c3b07fa686ae03060d12765b98
Abdurrahman-Kavak <apokvk03@gmail.com>
2026-05-26 13:52:42 +0300

    middleware yazıldı

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,6 @@
+const db = require("../../data/db-config");
+const schemes = require("./scheme-model");
+
 /*
   Eğer `scheme_id` veritabanında yoksa:
 
@@ -7,8 +10,18 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
-}
+  const { scheme_id } = req.params;
+  schemes.findById(scheme_id).then((scheme) => {
+    if (!scheme) {
+      req.scheme = scheme;
+      next();
+    } else {
+      res.status(404).json({
+        message: `scheme_id ${scheme_id} id li şema bulunamadı`,
+      });
+    }
+  });
+};
 
 /*
   Eğer `scheme_name` yoksa, boş string ya da string değil:
@@ -19,8 +32,15 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  const { scheme_name } = req.body;
+  if (scheme_name && typeof scheme_name === "string") {
+    next();
+  } else {
+    res.status(400).json({
+      message: "Geçersiz scheme_name",
+    });
+  }
+};
 
 /*
   Eğer `instructions` yoksa, boş string yada string değilse, ya da
@@ -32,11 +52,24 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
-}
+  const { instructions, step_number } = req.body;
+  if (instructions && typeof instructions === "string") {
+    if (step_number && typeof step_number === "number" && step_number > 0)
+      next();
+    else {
+      res.status(400).json({
+        message: "Hatalı step",
+      });
+    }
+  } else {
+    res.status(400).json({
+      message: "Hatalı step",
+    });
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
